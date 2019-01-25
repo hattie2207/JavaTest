2408abdefc927953280c24658b06abd0561cb4b0
Hattie Samankul <hattie.samankul@hotmail.com>
2019-01-25 15:28:56 +0000

    add methods to randomise location of treasure box

--- JavaTest/src/Player.java
+++ JavaTest/src/Player.java
@@ -1,36 +1,62 @@
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Player {
 
-	Scanner userInput = new Scanner(System.in);
-	String direction; 
-	float distance; 
-
+	private Scanner userInput = new Scanner(System.in);
+	private Scanner levelInput = new Scanner(System.in);
+	private String direction, gameLevel; 
+	private double distance;
+	private int pointX, pointY, randX, randY;
+	
 	public void play() {
 
+		randomPoint();
+		System.out.println("What level of adventure would you like to experience?"
+				+ "\nPlease enter \"easy\", \"intermediate\", or \"advance\".");
+		getLevel();
+		System.out.println("Grey foggy clouds float oppressively close to you,\r\n" + 
+				"reflected in the murky grey water which reaches up your shins.\r\n" + 
+				"Some black plants barely poke out of the shallow water.\r\n" + 
+				"Try \"north\",\"south\",\"east\",or \"west\"\r\n" + 
+				"You notice a small watch-like device in your left hand.\r\n" + 
+				"It has hands like a watch, but the hands don't seem to tell time.\r\n");	
+
+		totalDistance();
+		System.out.print("The dial reads '");
+		System.out.printf("%.3f", distance);
+		System.out.print("m'\n>");
 		playerMove();
-		magicCompass(direction);
 
-	}
-
-	public void playerMove() {
-
-		if (userInput.hasNext()) {
-
-			if (userInput.hasNextLine()) {
-				
-				direction = userInput.nextLine();	
-				checkInput(direction);
-			}
-		}		
 	}
 	
-	public void checkInput(String direction) {		
-		
+	private void randomPoint() {
+
+		do {
+
+			randX = ThreadLocalRandom.current().nextInt(-3, 3);
+			randY = ThreadLocalRandom.current().nextInt(-3, 3);		
+
+		}while ((randX == 0 || randY == 0));
+
+	}
+	
+	private void playerMove() {
+
+		if (userInput.hasNextLine()) {
+
+			direction = userInput.nextLine();	
+			checkInput(direction);
+		}
+	}		
+
+
+	private void checkInput(String direction) {		
+
 		direction = direction.toLowerCase();
-		
+
 		switch(direction) {
-		
+
 		case "n":
 			direction = "north";
 			break;
@@ -44,22 +70,20 @@ public class Player {
 			direction = "west";
 			break;
 		}
-		
+
 		if (direction.equals("north") || direction.equals("south") || direction.equals("east") || direction.equals("west")) {							
-			System.out.println("Valid Move");
+			magicCompass(direction);			
 		}					
 		else {
-			System.out.println("Invalid input, please try again."); 
+			System.out.println("Invalid move input, please try again.\n>"); 
 			playerMove();
 		}
-		
-	}
 
-	public String magicCompass(String direction) {
-		
-		
+	}		
+	
+	
 
-		return "";
-	}
+	
+}	
 
-}
+
